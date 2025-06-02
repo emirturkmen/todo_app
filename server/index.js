a53b03b9256cb0866bc48a6462c00c5ae51ce901
emirturkmen <e.turkmen@etu.edu.tr>
2025-06-02 23:10:13 +0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// server/index.js
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
